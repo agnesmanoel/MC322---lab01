0855402272e2f1eaf84156dc515705fedf997f9e
Beprados <beprados04@gmail.com>
2025-08-28 16:10:32 -0300

    finaliza o contrutor de MonstroAbstrato

--- lab01/src/MonstroAbstrato.java
+++ lab01/src/MonstroAbstrato.java
@@ -6,7 +6,11 @@ abstract class MonstroAbstrato extends PersonagemAbstrato{
     private int xpConcedido;
 
     public MonstroAbstrato(String n, int pv, int f, int xp) {
-        
+        super(n, pv, f);
+        this.xpConcedido = xp;
     }
 
+    // public void exibirStatus () {
+
+    // }
 }
